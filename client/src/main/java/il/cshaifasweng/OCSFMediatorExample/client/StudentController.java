d2ca84bcb2aec4bc75906a429ffa00c6cef55cfa
Tarek <tsaleh550@gmail.com>
2020-06-21 19:23:36 +0300

    stdent controller added

---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/StudentController.java
@@ -0,0 +1,78 @@
+package il.cshaifasweng.OCSFMediatorExample.client;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+public class StudentController  {
+
+	private  Scene scene;
+    private Stage stage;
+    
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ImageView welcomesign;
+
+    @FXML
+    private Button scanned;
+
+    @FXML
+    private Button grades;
+
+    @FXML
+    private Button execution;
+
+    @FXML
+    private Button logOut;
+
+    @FXML
+    void OnExecution(ActionEvent event) {
+    	try {
+			 scene=FXMLLoader.load(getClass().getResource("/client/src/main/resources/il/cshaifasweng/OCSFMediatorExample/client/examexec.fxml"));
+			 stage.setScene(scene);
+		     stage.show();
+		     System.out.print(event);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+
+    @FXML
+    void OnGrades(ActionEvent event) {
+    	
+    }
+
+    @FXML
+    void OnLogOut(ActionEvent event) {
+    	
+    }
+
+    @FXML
+    void OnScanned(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert welcomesign != null : "fx:id=\"welcomesign\" was not injected: check your FXML file 'student.fxml'.";
+        assert scanned != null : "fx:id=\"scanned\" was not injected: check your FXML file 'student.fxml'.";
+        assert grades != null : "fx:id=\"grades\" was not injected: check your FXML file 'student.fxml'.";
+        assert execution != null : "fx:id=\"execution\" was not injected: check your FXML file 'student.fxml'.";
+        assert logOut != null : "fx:id=\"logOut\" was not injected: check your FXML file 'student.fxml'.";
+
+    }
+}
