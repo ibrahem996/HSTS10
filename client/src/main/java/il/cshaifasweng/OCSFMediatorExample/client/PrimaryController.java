e175a1c445d3f33c3525f71b969993dac196e95f
Tarek <tsaleh550@gmail.com>
2020-06-21 22:02:54 +0300

    lll

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/PrimaryController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/PrimaryController.java
@@ -1,6 +1,6 @@
 package il.cshaifasweng.OCSFMediatorExample.client;
 
-import java.io.IOException; 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -14,94 +14,81 @@ import il.cshaifasweng.OCSFMediatorExample.client.App;
 
 public class PrimaryController {
 
-	
-    @FXML
-    private ResourceBundle resources;
+	@FXML
+	private ResourceBundle resources;
 
-    @FXML
-    private URL location;
+	@FXML
+	private URL location;
 
-    @FXML
-    private TextField usertxt;
+	@FXML
+	private TextField usertxt;
 
-    @FXML
-    private MenuButton interfaceid;
+	@FXML
+	private MenuButton interfaceid;
 
-    @FXML
-    private MenuItem studentid;
+	@FXML
+	private MenuItem studentid;
 
-    @FXML
-    private MenuItem teacherid;
+	@FXML
+	private MenuItem teacherid;
 
-    @FXML
-    private MenuItem managerid;
+	@FXML
+	private MenuItem managerid;
 
-    @FXML
-    private PasswordField passtxt;
+	@FXML
+	private PasswordField passtxt;
 
-    @FXML
-    private Button loginbtn;
-    
-    String userType;
-    
-    
+	@FXML
+	private Button loginbtn;
 
-    @FXML
-    void interfaceac(ActionEvent event) {
+	String userType;
 
-    }
+	@FXML
+	void interfaceac(ActionEvent event) {
 
-    @FXML
-    void loginac(ActionEvent event) throws IOException {
-    	
-    	String[] arr = { usertxt.getText() , passtxt.getText(),userType};
-    	
-    	App.getInstance().LoginIn(arr);
-    	
-    	
-    	System.out.println(usertxt.getText());
-    	System.out.println(passtxt.getText());
-    	System.out.println(userType);
+	}
 
-    }
+	@FXML
+	void loginac(ActionEvent event) throws IOException {
 
-    @FXML
-    void managerac(ActionEvent event) 
-    {
-    	userType = "Manager";
-    	interfaceid.setText("Manager");
-    }
+		String[] arr = { usertxt.getText(), passtxt.getText(), userType };
 
-    @FXML
-    void studentac(ActionEvent event)
-    {
+		App.getInstance().LoginIn(arr);
 
-    	userType = "Student";
-    	interfaceid.setText("Student");
-    }
+		System.out.println(usertxt.getText());
+		System.out.println(passtxt.getText());
+		System.out.println(userType);
 
-    @FXML
-    void teacherac(ActionEvent event)
-    {
-    	userType = "Teacher";
-    	interfaceid.setText("Teacher");
-    }
+	}
 
+	@FXML
+	void managerac(ActionEvent event) {
+		userType = "Manager";
+		interfaceid.setText("Manager");
+	}
 
+	@FXML
+	void studentac(ActionEvent event) {
 
-    @FXML
-    void initialize() {
-        assert usertxt != null : "fx:id=\"usertxt\" was not injected: check your FXML file 'primary.fxml'.";
-        assert interfaceid != null : "fx:id=\"interfaceid\" was not injected: check your FXML file 'primary.fxml'.";
-        assert studentid != null : "fx:id=\"studentid\" was not injected: check your FXML file 'primary.fxml'.";
-        assert teacherid != null : "fx:id=\"teacherid\" was not injected: check your FXML file 'primary.fxml'.";
-        assert managerid != null : "fx:id=\"managerid\" was not injected: check your FXML file 'primary.fxml'.";
-        assert passtxt != null : "fx:id=\"passtxt\" was not injected: check your FXML file 'primary.fxml'.";
-        assert loginbtn != null : "fx:id=\"loginbtn\" was not injected: check your FXML file 'primary.fxml'.";
+		userType = "Student";
+		interfaceid.setText("Student");
+	}
 
-    }
+	@FXML
+	void teacherac(ActionEvent event) {
+		userType = "Teacher";
+		interfaceid.setText("Teacher");
+	}
+
+	@FXML
+	void initialize() {
+		assert usertxt != null : "fx:id=\"usertxt\" was not injected: check your FXML file 'primary.fxml'.";
+		assert interfaceid != null : "fx:id=\"interfaceid\" was not injected: check your FXML file 'primary.fxml'.";
+		assert studentid != null : "fx:id=\"studentid\" was not injected: check your FXML file 'primary.fxml'.";
+		assert teacherid != null : "fx:id=\"teacherid\" was not injected: check your FXML file 'primary.fxml'.";
+		assert managerid != null : "fx:id=\"managerid\" was not injected: check your FXML file 'primary.fxml'.";
+		assert passtxt != null : "fx:id=\"passtxt\" was not injected: check your FXML file 'primary.fxml'.";
+		assert loginbtn != null : "fx:id=\"loginbtn\" was not injected: check your FXML file 'primary.fxml'.";
+
+	}
 }
-
-
-
-
--- server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/App.java
+++ server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/App.java
@@ -19,9 +19,15 @@ public class App
     public static void main( String[] args ) throws IOException, SQLException
     {
     	String name,pass,url;
+<<<<<<< HEAD
 		url = "jdbc:mysql://127.0.0.1/hsts";
 		name = "root";
 		pass = "t12345";
+=======
+		url = "jdbc:mysql://127.0.0.1/hstsdatabase";
+		name = "root";
+		pass = "Lilyan$4";
+>>>>>>> a9484d2740d5481d40aa5799c1af01e8804f4d9b
 		Connection myConnection = DriverManager.getConnection(url,name,pass);
         Statement stmt = (Statement) myConnection.createStatement();
         String details = "SELECT * FROM `question` WHERE `id`=2500";
