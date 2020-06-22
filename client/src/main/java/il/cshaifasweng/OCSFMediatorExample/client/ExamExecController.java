9e33bb6c5168e610d80be5b9855c89054f7d3d14
Lilyana Khatib <lilyana-kh-19@hotmail.com>
2020-06-22 14:00:40 +0300

    added exam controller

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/ExamExecController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/ExamExecController.java
@@ -1,5 +1,25 @@
 package il.cshaifasweng.OCSFMediatorExample.client;
 
+import java.awt.event.ActionEvent;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+
 public class ExamExecController {
+
+    @FXML
+    private TextField IDText;
+
+    @FXML
+    private TextField ExamCode;
+
+    @FXML
+    private Button StartButton;
+
+    @FXML
+    void OnStart(ActionEvent event) {
+
+    }
 
 }
